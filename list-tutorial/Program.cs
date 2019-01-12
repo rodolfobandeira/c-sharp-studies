028bd867e4a98474ccae7b3ff4d26506b6835a7e
Rodolfo <1811118+rodolfobandeira@users.noreply.github.com>
2019-01-12 10:58:33 -0500

    Initial commit

---
+++ list-tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Rodolfo", "Bandeira" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"{name.ToUpper()}!");
+            }
+        }
+    }
+}
